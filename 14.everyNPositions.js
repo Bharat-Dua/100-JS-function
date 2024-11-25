01a72738ebe946ad2b2f25f3ad5af92772b90bde
Bharat-Dua <bharatdua13@gmail.com>
2024-11-25 16:18:11 +0530

    solved

---
+++ 14.everyNPositions.js
@@ -0,0 +1,32 @@
+//* 14.everyNPositions
+
+//? Write a function named everyNPositions that receives 2 parameters:
+
+// a string - message
+// a number - step
+// and returns a new string composed of all characters from message that are on positions divisible by step.
+
+/* 
+Example 1
+Input
+
+message="Which framework should I choose?"
+
+step=6
+
+Output
+"Wfoo e"
+Explanation
+Our message has 32 characters. The characters on positions 0, 6, 12, 18, 24 and 30 make up the result Wfoo e.
+
+*/
+
+function everyNPositions(message, step) {
+  let resultStr = "";
+  for (let i = 0; i < message.length; i += step) {
+    resultStr += message[i];
+  }
+  return resultStr;
+}
+
+export { everyNPositions };
